e212e63d201b636062604be06737ea597e39a8e1
geohpassos <110428388+geohpassos@users.noreply.github.com>
2024-11-04 16:12:05 -0300

    exercicio 11

---
+++ exer11/exer11/exer11.js
@@ -0,0 +1,15 @@
+var num1 = parseInt(prompt("Digite o primeiro número: "))
+var num2 = parseInt(prompt("Digite o segundo número: "))
+var num3 = parseFloat(prompt("Digite o terceiro número: "))
+var produto
+var soma
+var aocubo
+
+produto = (num1*2)+(num2/2)
+alert("O produto do dobro do primeiro com metade do segundo é: "+produto)
+
+soma = (num1*3) + num3
+alert("A soma do triplo do primeiro com o terceiro: "+soma)
+
+aocubo = (num3*num3*num3)
+alert("O terceiro número ao cubo é: "+aocubo)
